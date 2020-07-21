dc5bf445b1d222dd46154cbc46f462bee26519ac
petra-vozarova <61162312+petra-vozarova@users.noreply.github.com>
2020-07-21 16:10:46 +0100

    Updates

---
+++ precache-manifest.4dedbefd0911d68da02e850082a2253b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18871b5d1b01fb8d795fd38b572c1c67",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "025b3bca5cb4512cb9fa",
+    "url": "/calculator/static/css/main.6c590a18.chunk.css"
+  },
+  {
+    "revision": "d5b1d29fe0bd750cc601",
+    "url": "/calculator/static/js/2.db8fa8da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.db8fa8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "025b3bca5cb4512cb9fa",
+    "url": "/calculator/static/js/main.79b4d0b4.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
